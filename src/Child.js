deccaed7305d18a9cc0fcd4a8963d4452aa72949
smithkashyapdev <smithkashyap8@gmail.com>
2022-07-15 12:11:21 +0530

    1 file modified

--- src/Child.js
+++ src/Child.js
@@ -10,51 +10,43 @@ import { useSelector } from 'react-redux';
 
 export default function Child(props) {
   const { posts, count } = useSelector((state) => {
-    console.log('child-', state);
+    console.log('child--', state);
     return state;
   });
-  // const { posts, loadingPosts } = useSelector((state) =>
-
-  // {
-  //   console.log('child->',state)
-  // return state
-  // }
-  // );
 
   const [first, setFirstInput] = useState(0);
   const [second, setSecondInput] = useState(0);
-  const [users, setUsers] = useState([]);
- 
+
   //const count = 2;
   const otherFoo = function () {
     return `bar`;
   };
 
-  const getPost = useMemo(() => {
-    return posts
-  }, []);
+  // const getPost = useMemo(() => {
+  //   return posts
+  // }, []);
 
-  function getData(array) {
-    return new Promise(function (myResolve, myReject) {
-      // "Producing Code" (May take some time)
-      const extracted = array.map((object) => {
-        return { name: object.title };
-      });
-      myResolve(extracted); // when successful
-      myReject('Not extracted'); // when error
-    });
-  }
+  // function getData(array) {
+  //   return new Promise(function (myResolve, myReject) {
+  //     // "Producing Code" (May take some time)
+  //     const extracted = array.map((object) => {
+  //       return { name: object.title };
+  //     });
+  //     myResolve(extracted); // when successful
+  //     myReject('Not extracted'); // when error
+  //   });
+  // }
 
-  getData(getPost).then(
-    function (value) {
-      console.log('pro', value);
-      setUsers(value.toString());
-    },
-    function (error) {
-      console.log(error)
-      /* code if some error */
-    }
-  );
+  // getData(getPost).then(
+  //   function (value) {
+  //     console.log('pro', value);
+  //     setUsers(value.toString());
+  //   },
+  //   function (error) {
+  //     console.log(error)
+  //     /* code if some error */
+  //   }
+  // );
 
   //console.log('again render');
   return (
@@ -62,7 +54,7 @@ export default function Child(props) {
       <h1>{props.name}</h1>
       <p>{count}</p>
       <p>{props.memo}</p>
-     
+
       <input
         type="number"
         onChange={(e) => {
